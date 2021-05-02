5f4fdb9d059c5881cd8535f9f823c56d3d2409fa
Christopher Roberts <10robertsc@gmail.com>
2021-05-02 14:01:49 +0200

    'Hello world' for .ppm image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::stderr;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let width = 256;
+    let height = 256;
+
+    print!("P3\n{} {}\n255\n", width, height);
+
+    let blue = 63;
+    for row in 0..height {
+        eprint!("\rScanlines remaining: {}", height - 1 - row);
+        stderr().flush().unwrap();
+        for col in 0..width {
+            let red = col;
+            let green = width - row + 1;
+            println!("{} {} {}", red, green, blue);
+        }
+    }
 }
